b49e2414114b3933295b6a455e73cd415851783b
Evan Marshall <contact@elysianwebdesign.com>
2017-10-14 23:01:58 -0300

    finishing setup, fix testimonials and footer section.

--- src/js/custom.js
+++ src/js/custom.js
@@ -39,7 +39,35 @@ $(document).ready(function(){
 		slidesToScroll: 4,
 		autoplay: true,
 		autoplaySpeed: 4000,
-		infinite: true
+		infinite: true,
+		responsive: [
+    	{
+      		breakpoint: 1024,
+      		settings: {
+        		slidesToShow: 3,
+        		slidesToScroll: 3,
+        		infinite: true,
+        		dots: true
+      		}
+    	},
+    	{
+      		breakpoint: 600,
+      		settings: {
+        		slidesToShow: 1,
+        		slidesToScroll: 1
+      		}
+    	},
+    	{
+      		breakpoint: 480,
+      		settings: {
+        		slidesToShow: 1,
+        		slidesToScroll: 1
+      		}
+    	}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+  		]
   	});
 	
 });
